ede305afb66a2836869e201afcd5b859bf474df2
qinhui_wang <qaddafi2008@126.com>
2012-07-29 16:46:33 +0800

    delete the files that were not used

--- webApp/Runtime/Data/_fields/pkucxcy.incubator.php
+++
@@ -1,2 +0,0 @@
-<?php
-return array ( 0 => 'incubatorid', 1 => 'incubatorname', 2 => 'incubatorcontent', '_autoinc' => true, '_pk' => 'incubatorid', ); ?>
--- webApp/Runtime/Data/_fields/pkucxcy.incubatormodules.php
+++
@@ -1,2 +0,0 @@
-<?php
-return array ( 0 => 'moduleid', 1 => 'incubatorno', 2 => 'modulename', 3 => 'modulecontent', '_autoinc' => true, '_pk' => 'moduleid', ); ?>
