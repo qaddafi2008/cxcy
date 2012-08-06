d67d23ff2b2011db906d47f5b55d2d3eaa1ae11e
Charles <chencaixing@gmail.com>
2012-08-06 13:07:29 +0800

    1、修改home前台page左侧一级菜单为二级，同时修改后台菜单的生成代码 2、增加左侧菜单的滚动条跟随 3、增加左侧sidebar菜单点击时的内容显示

--- Public/js/page.js
+++ Public/js/page.js
@@ -1,32 +1,48 @@
 $(document).ready(function() {
-	var href = window.location.href;
-	var sidebarindex = -1;
+	
+	var href = window.location.href;//当前url
+	var ishomepage = -1;//当前homepage的index
 	$.each($("#menu>li>a"), function(i,item){  
 			$(this).attr("class","but");
 			if(href.indexOf($(this).attr("href"))!=-1){
+				ishomepage = i;
 				$(this).attr("class","but_active");
 			}
 		});
-	$.each($("#sidebar a"),function(i,item){
+	if(ishomepage==-1)
+	{
+		$("#menu>li>a:first").attr("class","but_active");
+	}
+	
+	//sidebar一级菜单跟随点击的颜色变化
+	$.each($("#list>li>a"),function(i,item){
+		$(this).removeClass("color_active");
+		$(this).parent("li").addClass("hover");
 		if(href.indexOf($(this).attr("href"))!=-1){
-			sidebarindex = i;
-		}
-	});
-	$.each($("#sidebar li:even"),function(i,item){
-		$(this).attr("class","color");
-		if((2*i)==sidebarindex)
-		{
-			$(this).attr("class","color_active");
-		}
-	});
-	$.each($("#sidebar li:odd"),function(i,item){
-		$(this).attr("class","");
-		if((2*i+1)==sidebarindex)
-		{
-			$(this).attr("class","color_active");
+			$(this).addClass("color_active");
+			$(this).parent("li").removeClass("hover");
+			$(this).parent("li").children("dl").show();
 		}
 	});
 	
+	//sidebar二级菜单跟随点击的颜色变化
+	$.each($("#list>li>dl>dd>a"),function(i,item){
+		$(this).click(
+				function(){
+					$.each($("#list>li>dl>dd>a"),function(i,item)
+					{
+						$(this).removeClass("color_active");
+					});
+					$(this).addClass("color_active");
+					$(this).parent("dd").parent("dl").parent("li").removeClass("hover");
+					$(this).parent("dd").parent("dl").parent("li").children("a").addClass("color_active");
+					$(this).parent("dd").parent("dl").parent("li").children("dl").show();
+				
+				}
+		);
+	});
+	
+	//登陆框
 	$(".signin").click(function(e) {
 		e.preventDefault();
 		$("fieldset#signin_menu").toggle();
@@ -43,6 +59,36 @@ $(document).ready(function() {
 		}
 	});
 
+	//下拉式菜单，主菜单
+	$("#menu li").hover(function() {
+		$(this).children("dl").slideDown(100)
+	}, function() {
+		$(this).children("dl").slideUp(100)
+	});
+	
+	//下拉式菜单，sidebar菜单
+	$("#list .hover").hover(function() {
+		$(this).children("dl").slideDown(100)
+	}, function() {
+		$(this).children("dl").slideUp(100)
+	}); 
+	
+	
+	//sidebar跟随滚动条滚动
+	var basetop = $("#main_block").offset().top ;//sidebar的基准top
+	$(window).scroll(function (){ 
+		var scrolltop = $(window).scrollTop();
+		if(scrolltop>basetop)
+		{
+			$("#sidebar").animate({top : scrolltop +"px" },{ duration:300 , queue:false });
+		}
+		else
+		{
+			$("#sidebar").animate({top : basetop+"px" },{ duration:300 , queue:false });
+			
+		}
+	}); 
+	
 });
 $(function() {
 	$('#forgot_username_link').tipsy({
@@ -50,3 +96,14 @@ $(function() {
 	});
 });
 
+//腾飞创业部分首页的页面内跳转
+function forwardto(dest)
+{
+	$.each($("#main_block .title"),function(i,item){
+		if($(this).text()==dest)
+		{
+			var scrolltop = $(this).offset().top+"px";
+			$( 'html, body' ).animate( { scrollTop: scrolltop }, { duration:300 , queue:false } );
+		}
+	});
+}
--- webApp/Lib/Action/Home/IndexAction.class.php
+++ webApp/Lib/Action/Home/IndexAction.class.php
@@ -1,193 +1,157 @@
 <?php
 
 class IndexAction extends Action {
-    public function index()
-	{
-        //header("Content-Type:text/html; charset=utf-8");
-        //echo '<div style="font-weight:normal;color:blue;float:left;width:345px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;font-size:14px;font-family:Tahoma">^_^ Hello,北京欢迎您！<span style="font-weight:bold;color:red">创新创业平台</span></div>';
-		$this->display();
+	public function index() {
+		//header("Content-Type:text/html; charset=utf-8");
+		//echo '<div style="font-weight:normal;color:blue;float:left;width:345px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;font-size:14px;font-family:Tahoma">^_^ Hello,北京欢迎您！<span style="font-weight:bold;color:red">创新创业平台</span></div>';
+		$this->display ();
 	}
 	
-	public function login()
-	{
+	public function login() {
 		//echo .'-'.$_POST[''].'-'.$_POST['usertype'];
 		
-		
-		if('teacher' ==$_POST['usertype'])
-		{
-			$condition['usrname']= $_POST['username'];
-			$condition['password']= md5($_POST['password']);
-			$condition['role']= 1;
+
+		if ('teacher' == $_POST ['usertype']) {
+			$condition ['usrname'] = $_POST ['username'];
+			$condition ['password'] = md5 ( $_POST ['password'] );
+			$condition ['role'] = 1;
 			
-			$stuff = M('stuff');
-			$checkTeacher = $stuff->where($condition)->find();
-			if(!$checkTeacher)
-			{
-				$this->error("用户或密码错误！");
-			}else
-			{
-				session(C('USER_AUTH_KEY'),$checkTeacher['stuffid']);//USER_AUTH_KEY 为用户id
-				session(C('USER_NAME'),$checkTeacher['usrname']);//USER_NAME为用户姓名
-				session(C('USER_ROLE'),$checkTeacher['role']);//USER_ROLE为用户角色，0为管理员，1为评审老师，2为学生
-			
-			
-				$this->success('登陆成功','index');
+			$stuff = M ( 'stuff' );
+			$checkTeacher = $stuff->where ( $condition )->find ();
+			if (! $checkTeacher) {
+				$this->error ( "用户或密码错误！" );
+			} else {
+				session ( C ( 'USER_AUTH_KEY' ), $checkTeacher ['stuffid'] ); //USER_AUTH_KEY 为用户id
+				session ( C ( 'USER_NAME' ), $checkTeacher ['usrname'] ); //USER_NAME为用户姓名
+				session ( C ( 'USER_ROLE' ), $checkTeacher ['role'] ); //USER_ROLE为用户角色，0为管理员，1为评审老师，2为学生
+				
+
+				$this->success ( '登陆成功', 'index' );
 			}
-		}else//type is student
-		{
-			$condition['stuno']= $_POST['username'];
-			$condition['spassword']= md5($_POST['password']);
+		} else //type is student
+{
+			$condition ['stuno'] = $_POST ['username'];
+			$condition ['spassword'] = md5 ( $_POST ['password'] );
 			
-			$student = M('student');
-        	$checkStudent = $student->where($condition)->find();
-			if(!$checkStudent)
-			{
-				$this->error("用户或密码错误！");
-			}else
-			{
-				session(C('USER_AUTH_KEY'),$checkStudent['sid']);//USER_AUTH_KEY 为用户id
-				session(C('USER_NAME'),$checkStudent['sname']);//USER_NAME为用户姓名
-				session(C('USER_ROLE'),2);//USER_ROLE为用户角色，0为管理员，1为评审老师，2为学生
-			
-			
-				$this->success('登陆成功','index');
+			$student = M ( 'student' );
+			$checkStudent = $student->where ( $condition )->find ();
+			if (! $checkStudent) {
+				$this->error ( "用户或密码错误！" );
+			} else {
+				session ( C ( 'USER_AUTH_KEY' ), $checkStudent ['sid'] ); //USER_AUTH_KEY 为用户id
+				session ( C ( 'USER_NAME' ), $checkStudent ['sname'] ); //USER_NAME为用户姓名
+				session ( C ( 'USER_ROLE' ), 2 ); //USER_ROLE为用户角色，0为管理员，1为评审老师，2为学生
+				
+
+				$this->success ( '登陆成功', 'index' );
 			}
 		}
 	}
 	
-	public function logout(){
-		session(null);//清空当前的session
-		$this->display('index');
+	public function logout() {
+		session ( null ); //清空当前的session
+		$this->display ( 'index' );
 	}
 	
-	public function insert(){
-		$Demo = new Model('student');
-		$Demo->Create();
-		$result = $Demo->add();
-		$this->redirect('user');
+	public function insert() {
+		$Demo = new Model ( 'student' );
+		$Demo->Create ();
+		$result = $Demo->add ();
+		$this->redirect ( 'user' );
 	}
 	
-	
-	public function page()
-	{
-		$maincontent[0]=array(
-			'title'=>'测试标题',
-			'content'=>'测试内容测试内容测试内容测试内容测试内容测试内容'
-		);
-		$this->assign('maincontent',$maincontent);
-		$this->display("page");
+	public function page() {
+		$maincontent [0] = array ('title' => '测试标题', 'content' => '测试内容测试内容测试内容测试内容测试内容测试内容' );
+		$this->assign ( 'maincontent', $maincontent );
+		$this->display ( "page" );
 	}
-	
 	
 	//新闻列表测试
-	public function index3()
-	{
+	public function index3() {
 		
-		$list[0] = array(
-			"url"=>'__URL__/index',
-			"title"=>'testtesttesttesttest',
-			"time"=>'2011年9月10日'
-		);
-		$list[1] = array(
-			"url"=>'',
-			"title"=>'testtesttesttesttest'
-		);
-		$list[2] = array(
-			"url"=>'',
-			"title"=>'testtesttesttesttest'
-		);
-		$list[3] = array(
-			"url"=>'',
-			"title"=>'testtesttesttesttest'
-		);
+		$list [0] = array ("url" => '__URL__/index', "title" => 'testtesttesttesttest', "time" => '2011年9月10日' );
+		$list [1] = array ("url" => '', "title" => 'testtesttesttesttest' );
+		$list [2] = array ("url" => '', "title" => 'testtesttesttesttest' );
+		$list [3] = array ("url" => '', "title" => 'testtesttesttesttest' );
 		
-		$this->assign('newslist',$list);
-		$this->display("page");
+		$this->assign ( 'newslist', $list );
+		$this->display ( "page" );
 	}
-	
 	
 	//腾飞创业模块
-	public function tengfei()
-	{	
-		unset($_GET['_URL_']);
-		
+	public function tengfei() {
+		unset ( $_GET ['_URL_'] );
+		//var_dump($_GET);
 		//左侧导航栏
-		$modelname = "腾飞创业：";//本模块名称
-		$navlist[0] = array(
-			"url"=>'__URL__/tengfei/type/rhtf',
-			"title"=>'如何腾飞',
-		);
-		$navlist[1] = array(
-			"url"=>'__URL__/tengfei/type/tfzw',
-			"title"=>'腾飞展望',
-		);
+		$modelname = "腾飞创业："; //本模块名称
+		$navlist [0] = array ("url" => '__URL__/tengfei/rhtf/view', "title" => '如何腾飞' );
+		$navlist [1] = array ("url" => '__URL__/tengfei/tfzw/view', "title" => '腾飞展望' );
+		$navlist [1] ['subnav'] = array (
+				"0" => array (
+						"url" => 'javascript:forwardto(\'推介交流\')', 
+						"title" => '推介交流' 
+				), 
+				"1" => array (
+						"url" => 'javascript:forwardto(\'服务概述\')',
+						"title" => '服务概述'
+				),
+				"2" => array (
+						"url" => 'javascript:forwardto(\'团队义务\')',
+						"title" => '团队义务' 
+				) );
 		
-		$maincontent = null;
-		$condition = null;
 		//获取参数
-		$type = $_GET['type'];
-		
-		
+		$type = null;
+		if (isset ( $_GET ['rhtf'] )) {
+			$type = 1; //如何腾飞
+		} elseif (isset ( $_GET ['tfzw'] )) {
+			$type = 2; //腾飞展望
+		} else
+			$type = 3; //本模块首页介绍
+
 		//从数据库转存信息
 		$num = 0;
-		if($type=="rhtf"||$type=="tfzw")//如何腾飞和腾飞展望
-		{
-			//数据库
-			$incubator = M('incubator');
-			$incubatormodules = M('incubatormodules');
+		$maincontent = null;
 		
-			//查询incubator表
-			$condition1['incubatorid'] = $type=="rhtf"?1:2;
-			$temp = $incubator->where($condition1)->select();
-			foreach($temp as $one=>$value)
-			{
-				$maincontent[$num++] = array(
-					'title' => $value[incubatorname],
-					'content' => $value[incubatorcontent],
-				);
-			}
-			
-			//查询incubatormodules表
-			$condition2['incubatorno'] = $type=="rhtf"?1:2;
-			$temp = $incubatormodules->where($condition2)->select();
-			foreach($temp as $one=>$value)
-			{
-				$maincontent[$num++] = array(
-					'title' => $value[modulename],
-					'content' => $value[modulecontent],
-				);
-			}
-		}
-		else//首页进来为该孵化器平台的简介
-		{
-			//数据库
-			$incubator = M('incubator');
-			$condition['incubatorid'] = '3';
-			$temp = $incubator->where($condition)->select();
-			foreach($temp as $one=>$value)
-			{
-				$maincontent[$num++] = array(
-					'title' => $value[incubatorname],
-					'content' => $value[incubatorcontent],
-				);
-			}
+		//数据库
+		$incubator = M ( 'incubator' );
+		$incubatormodules = M ( 'incubatormodules' );
+		
+		//查询incubator表
+		$condition1 ['incubatorid'] = $type;
+		$temp = $incubator->where ( $condition1 )->select ();
+		foreach ( $temp as $one => $value ) {
+			$maincontent [$num ++] = array (
+					'title' => $value [incubatorname], 
+					'content' => $value [incubatorcontent]
+			);
 		}
 		
-		$this->assign('modelname',$modelname);
-		$this->assign('navlist',$navlist);
-		$this->assign('maincontent',$maincontent);
-		$this->display("page");
+		//查询incubatormodules表
+		$condition2 ['incubatorno'] = $type;
+		$temp = $incubatormodules->where ( $condition2 )->select ();
+		foreach ( $temp as $one => $value ) {
+			$maincontent [$num ++] = array (
+					'title' => $value [modulename], 
+					'content' => $value [modulecontent]
+			);
+		}
+		
+		$this->assign ( 'modelname', $modelname );
+		$this->assign ( 'navlist', $navlist );
+		$this->assign ( 'maincontent', $maincontent );
+		$this->display ( "page" );
 	}
 	
-	public function user(){
-		$demo = M('student');
-		$list=$demo->select();
-		$this->assign('list',$list);
-		$this->display("index:user");
+	public function user() {
+		$demo = M ( 'student' );
+		$list = $demo->select ();
+		$this->assign ( 'list', $list );
+		$this->display ( "index:user" );
 	}
 	
-	public function cyk(){
-		echo 'msg is: '.$_GET['msg'].'<br/>secondmsg:'.$_GET['secondmsg'];
+	public function cyk() {
+		echo 'msg is: ' . $_GET ['msg'] . '<br/>secondmsg:' . $_GET ['secondmsg'];
 	}
-	
+
 }
