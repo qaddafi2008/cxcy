093c546092218ce27c094fbedc0ec204eb3da5b6
Charles <chencaixing@gmail.com>
2012-08-20 00:02:28 +0800

    1、更新部分leftframe 2、更新competitionaction的处理

--- webApp/Lib/Action/Admin/CompetitionAction.class.php
+++ webApp/Lib/Action/Admin/CompetitionAction.class.php
@@ -1,18 +1,24 @@
 <?php
 
 class CompetitionAction extends Action {
-	
+
 	public function detail() {
 		//var_dump($_GET);
-		$module = $_GET ['_URL_'] [3];
-		$submodule = $_GET ['_URL_'] [4];
-		if ($module == "cxss") {
-			$contition = array ('comtype' => 1 );
+		$module = $_GET ['_URL_'] [3];//主模块，是创新赛事，或者合河赛事
+		$submodule = $_GET ['_URL_'] [4];//子模块，代表阶段设置
+		if(!$submodule||$submodule==null||$submodule==""){//设置主模块，故子模块为空
+			if ($module == "cxss") {
+				$condition = array ('comtype' => 1 );
+			}
+			else if($module == "hhss"){
+				$condition = array ('comtype' => 2 );
+			}
 			$competitions = M ( 'competitions' );
-			$temp = $competitions->where ( $condition )->select ();
-			$this->assign ( "competition", $temp [0] );
-		
-		//echo $module;
+			$temp = $competitions->where ( $condition )->find ();
+			$this->assign ( "competition", $temp);
+		}
+		else if($submodule=="proc"){
+			
 		}
 		$this->display ();
 	}
@@ -20,7 +26,7 @@ class CompetitionAction extends Action {
 		//print_r($_FILES);
 		$module = $_GET ['_URL_'] [3];
 		$submodule = $_GET ['_URL_'] [4];
-		if ($module == "cxss" && ! $submodule) {
+		if (!$submodule||$submodule==null||$submodule=="") {
 			$competition = array ('comid' => $_POST ["comid"], 'subject' => $_POST ["subject"], 'createtime' => $_POST ['createtime'], 'description' => $_POST ['description'] );
 			if (! isset ( $_POST ['alreadyfile'] )) {
 				$competition ['attachmentpath'] = "";
@@ -38,15 +44,15 @@ class CompetitionAction extends Action {
 			// 错误后返回错误的操作状态和提示信息
 			$this->ajaxReturn ( 0, "(未更新！)", 0 );
 		}
-	
+
 		//$this->display("view");
-	//redirect(U('/Admin/Tengfei/view/mid/3'), 5, '页面跳转中...');
+		//redirect(U('/Admin/Tengfei/view/mid/3'), 5, '页面跳转中...');
 	}
-	
+
 	protected function upload() {
-		
+
 		if (($_FILES ["file"] ["size"] < 10 * 1024 * 1024)) {
-			
+
 			if ($_FILES ["file"] ["error"] > 0) {
 				//echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
 				return ERROR;
