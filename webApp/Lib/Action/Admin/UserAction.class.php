3c0c37d07c349dd011a1f28a0d91ee6a6085b864
qinhui_wang <qaddafi2008@126.com>
2012-08-21 23:56:05 +0800

    1.完成修改密码与二次密码功能； 2.完成管理员、老师列表显示； 3.完成学生列表分页显示，以及单个的删除功能

--- webApp/Lib/Action/Admin/UserAction.class.php
+++ webApp/Lib/Action/Admin/UserAction.class.php
@@ -27,6 +27,7 @@ class UserAction extends Action {
 		$this->display();
 	}
 	
+	//验证二次密码
 	public function doSecondPswdVerify(){
 		$condition['secondpwd'] = md5($_POST['secpswd']);
 		$condition['stuffid'] = session ( C ( 'USER_AUTH_KEY' ));
@@ -39,6 +40,12 @@ class UserAction extends Action {
 		}
 	}
 	
+	public function createAdministrator(){
+		R('Admin/Authority/checkAdminLogin');//表示调用Admin分组下Authority模块的checkAdminLogin方法
+		$this->display();
+	}
+	
+	//创建管理员账号
 	public function addAdministrator(){
 		$stuff = M('stuff');
 		$condition['usrname']=$_POST['usrname'];
@@ -55,6 +62,85 @@ class UserAction extends Action {
 			$stuff->add($data);
 			$this->success ( '创建管理员账号成功!','createAdministrator');
 		}
+	}
+	
+	public function changePassword(){
+		R('Admin/Authority/checkAdminLogin');//表示调用Admin分组下Authority模块的checkAdminLogin方法
+		if(0 == $_GET['type'])//修改密码
+			$this->display();
+		else 				  //修改二次密码
+			$this->display("changeSecondPassword");
+	}
+	
+	//修改密码或者二次密码
+	public function doChangePassword(){
+		$condition['stuffid'] = session ( C ( 'USER_AUTH_KEY' ));
+		$stuff = M('stuff');
+		if(0 == $_POST['type']){//修改密码
+			$condition['password'] = md5($_POST['password']);
+			if( !$stuff->where($condition)->find())
+				$this->error ( "原密码错误，请重输入！" );
+			else{
+				$data['password'] = md5($_POST['newPassword']);
+			}
+		}else{					//修改二次密码
+			$condition['secondpwd'] = md5($_POST['password']);
+			if( !$stuff->where($condition)->find())
+				$this->error ( "原二次密码错误，请重输入！" );
+			else{
+				$data['secondpwd'] = md5($_POST['newPassword']);
+			}
+		}
+		
+		if($stuff->where("stuffid = ".$condition['stuffid'])->save($data))
+			$this->success("修改成功！");
+		else
+			$this->error("修改失败！");
+	}
+	
+	//分页显示学生信息
+	public function studentPage(){
+		R('Admin/Authority/checkAdminLogin');//表示调用Admin分组下Authority模块的checkAdminLogin方法
+		$student = M('student');
+		$list = $student->order('sid')->page($_GET['p'].',5')->select();
+		$this->assign('list',$list);
+		import("ORG.Util.Page");
+		$count = $student->count();
+		$page = new Page($count,5);
+		
+		$page->setConfig('prev','<img src="__PUBLIC__/images/admin/images/back.gif" width="43" height="15" />');
+		$page->setConfig('next','<img src="__PUBLIC__/images/admin/images/next.gif" width="43" height="15" />');
+		$gotoPage = '<span style="position:relative; bottom:3px">转到第<input name="textfield" type="text" style="height:20px; width:30px;"/>页</span>'.
+            '<a href="#"><img src="__PUBLIC__/images/admin/images/go.gif" width="37" height="15" /></a>';
+		$page->setConfig('theme','<span style="float:left">共%totalRow%%header%，当前第%nowPage%/%totalPage%页，每页10条记录</span> <span style="float:right">%upPage% %downPage% %first% %prePage% %linkPage%'.$gotoPage.'</span>');
+		
+		$show = $page->show();
+		$this->assign('page',$show);
+		$this->display();
+	}
+	
+	public function deleteStudent(){
+		$condition['sid'] = $_GET['sid'];
+		$student = M('student');
+		
+		if($student->where($condition)->delete())
+			$this->success("删除学生账号成功！");
+		else
+			$this->error("删除失败！");
+	}
+	
+	public function teacherList(){
+		$stuff = M('stuff');
+		$list = $stuff->where('role=1')->order('stuffid')->select();
+		$this->assign('list',$list);
+		$this->display();
+	}
+	
+	public function administratorList(){
+		$stuff = M('stuff');
+		$list = $stuff->where('role=0')->order('stuffid')->select();
+		$this->assign('list',$list);
+		$this->display();
 	}
 }
 ?>
