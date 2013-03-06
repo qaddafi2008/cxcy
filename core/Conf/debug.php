4cb3dd5d7753f87e83e7c769e0d497ecf0ba201f
qaddafi2008 <qaddafi2008@126.com>
2013-03-06 22:59:55 +0800

    1.修改坑爹的include中变量的问题 2.开启缓存功能

--- core/Conf/debug.php
+++ core/Conf/debug.php
@@ -32,5 +32,5 @@ return  array(
     'TMPL_CACHE_ON'    => false,        // 是否开启模板编译缓存,设为false则每次都会重新编译
     'TMPL_STRIP_SPACE'      => false,       // 是否去除模板文件里面的html空格与换行
     'SHOW_ERROR_MSG'        => true,    // 显示错误信息
-	'TMPL_CACHE_ON'=>false,      // 默认开启模板缓存
+	'TMPL_CACHE_ON'=>true,      // 默认开启模板缓存
 );
